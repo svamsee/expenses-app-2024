980a315739c3267732577c2cdca8deb35d08431c
VAMSEE_SURFACE\vamsi <vamsi.flex@gmail.com>
2024-03-02 12:30:19 +0530

    code refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,9 @@ import {
   signInWithPopup,
   user,
   signOut,
+  User,
 } from '@angular/fire/auth';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +16,15 @@ import {
 })
 export class AppComponent {
   private auth: Auth = inject(Auth);
-  user$ = user(this.auth);
+  user$: Observable<{
+    isLogin: boolean;
+    user: User | null;
+  }> = user(this.auth).pipe(
+    map((user) => ({
+      isLogin: !!user,
+      user,
+    }))
+  );
   loginWithGoogle() {
     signInWithPopup(this.auth, new GoogleAuthProvider());
   }
